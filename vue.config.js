b4d43c6fef19a3fd3ff6b6ff7bed6ea15f292a00
onlyeat3 <onlyeat3@gmail.com>
2021-11-24 14:10:59 +0800

    add NSIS配置

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,16 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       externals: ['iohook'],
-      nodeIntegration: true
+      nodeIntegration: true,
+      builderOptions: {
+        nsis: {
+          oneClick: false, // 是否一键安装
+          allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
+          createDesktopShortcut: true, // 创建桌面图标
+          createStartMenuShortcut: true,// 创建开始菜单图标
+        },
+      }
     }
   }
 }
